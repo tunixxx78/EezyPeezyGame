47ad9a048e2d52dd54cff4b0baa8133e2e24235f
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-05-26 17:01:06 +0300

    Just a little fix for avatar functionality

--- EezyPeezyGame/Assets/Scripts/TuronScripts/AvatarSystem.cs
+++ EezyPeezyGame/Assets/Scripts/TuronScripts/AvatarSystem.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AvatarSystem : MonoBehaviour
 {
     [SerializeField] GameObject[] avatarPrefabs;
-    [SerializeField] GameObject spawnpointForAvatar;
+    
     private void Start()
     {
         LoadAvatar();
@@ -14,6 +15,10 @@ public class AvatarSystem : MonoBehaviour
     void LoadAvatar()
     {
         int robotIndex = PlayerPrefs.GetInt("RobotIndex");
-        GameObject.Instantiate(avatarPrefabs[robotIndex]);
+        GameObject.Instantiate(avatarPrefabs[robotIndex], transform.position, Quaternion.identity);
+    }
+    public void GoGoGo()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
