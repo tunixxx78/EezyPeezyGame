fe68a8f5491bffea53d1e21b190e2c6008bc77e3
Taru Vikström <taru.vikstrom@yahoo.com>
2021-06-01 19:24:43 +0300

    Default canvas added, confirmation panel and buttons for going into the rocket
    Made a prefab of the basic canvas that can be used in all game views.  Clicking the Rocket door opening will pop a confirmation panel. Yes button changes scene to "rocket", no closes the panel.

--- EezyPeezyGame/Assets/Scripts/RocketDoorSlider.cs
+++ EezyPeezyGame/Assets/Scripts/RocketDoorSlider.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RocketDoorSlider : MonoBehaviour
 {
+    public GameObject confirmationPanel;
     /*This script is for Rockets doors to open when mouse is over the door collider
     */
     private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        confirmationPanel.SetActive(false);
         animator = gameObject.GetComponent<Animator>();
         animator.SetBool("Open", false);
     }
@@ -30,6 +34,14 @@ public class RocketDoorSlider : MonoBehaviour
             animator.Play("RocketDoors");
             animator.SetBool("Open", true);
         }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Debug.Log("klik");
+            confirmationPanel.SetActive(true);
+            StartCoroutine(ConfirmationWait());
+        }
+
     }
 
     private void OnMouseExit()
@@ -44,5 +56,21 @@ public class RocketDoorSlider : MonoBehaviour
             animator.SetBool("Open", false);
         }
 
+    }
+
+    IEnumerator ConfirmationWait()
+    {
+        yield return new WaitForSeconds(7f);
+        confirmationPanel.SetActive(false);
+    }
+
+    public void GoToRocket()
+    {
+        SceneManager.LoadScene("Rocket");
+    }
+
+    public void CloseConfirmationPanel()
+    {
+        confirmationPanel.SetActive(false);
     }
 }
