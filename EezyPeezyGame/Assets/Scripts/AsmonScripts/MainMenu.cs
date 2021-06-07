479462814fb4c960f789bb55c58991ac578c2f03
Taru Vikström <taru.vikstrom@yahoo.com>
2021-06-07 09:25:14 +0300

    Revert "start game leads to HomePlanet"
    This reverts commit c17886527f1ec6fc4cef5587caf8d32c959293a2.

--- EezyPeezyGame/Assets/Scripts/AsmonScripts/MainMenu.cs
+++ EezyPeezyGame/Assets/Scripts/AsmonScripts/MainMenu.cs
@@ -24,7 +24,7 @@ public class MainMenu : MonoBehaviour
      
     public void StartGame()
     {
-        SceneManager.LoadScene("HomePlanet");
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void QuitGame()
