d4a8063f130227b6db7374f8c9a7271aaaa89070
centyh <97asmo@gmail.com>
2021-06-18 15:03:07 +0300

    Simon Says Proto

---
+++ EezyPeezyGame/Assets/Scripts/AsmonScripts/SimonSays/GameManager.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public SpriteRenderer[] colors;
+
+    private int colorSelect;
+
+    public float stayLit;
+    private float stayLitCounter;
+
+    public float waitBetweenLights;
+    private float waitBetweenCounter;
+
+    private bool shouldBeLit;
+    private bool shouldBeDark;
+
+    public List<int> activeSequence;
+    private int positionInSequence;
+    private int inputInSequence;
+
+    private bool gameActive = false;
+    
+
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(shouldBeLit)
+        {
+            stayLitCounter -= Time.deltaTime;
+
+            if(stayLitCounter < 0)
+            {
+                colors[activeSequence[positionInSequence]].color = new Color(colors[activeSequence[positionInSequence]].color.r, colors[activeSequence[positionInSequence]].color.g, colors[activeSequence[positionInSequence]].color.b, 0.5f);
+                shouldBeLit = false;
+
+                shouldBeDark = true;
+                waitBetweenCounter = waitBetweenLights;
+
+                positionInSequence++;
+            }
+        }
+        if (shouldBeDark)
+        {
+            waitBetweenCounter -= Time.deltaTime;
+
+            if(positionInSequence >= activeSequence.Count)
+            {
+                shouldBeDark = false;
+                gameActive = true;
+            }
+            else
+            {
+                if(waitBetweenCounter < 0)
+                {
+
+
+                    colors[activeSequence[positionInSequence]].color = new Color(colors[activeSequence[positionInSequence]].color.r, colors[activeSequence[positionInSequence]].color.g, colors[activeSequence[positionInSequence]].color.b, 1f);
+
+                    stayLitCounter = stayLit;
+                    shouldBeLit = true;
+                    shouldBeDark = false;
+                }
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        activeSequence.Clear();
+
+        positionInSequence = 0;
+        inputInSequence = 0;
+
+        colorSelect = Random.Range(0, colors.Length);
+
+        activeSequence.Add(colorSelect);
+
+        colors[activeSequence[positionInSequence]].color = new Color(colors[activeSequence[positionInSequence]].color.r, colors[activeSequence[positionInSequence]].color.g, colors[activeSequence[positionInSequence]].color.b, 1f);
+
+        stayLitCounter = stayLit;
+        shouldBeLit = true;
+    }
+
+    public void ColorPressed(int whichButton)
+    {
+        if (gameActive)
+        {
+            if (activeSequence[inputInSequence] == whichButton)
+            {
+                Debug.Log("Correct");
+
+                inputInSequence++;
+
+                if(inputInSequence >= activeSequence.Count)
+                {
+                    positionInSequence = 0;
+                    inputInSequence = 0;
+
+                    colorSelect = Random.Range(0, colors.Length);
+
+                    activeSequence.Add(colorSelect);
+
+                    colors[activeSequence[positionInSequence]].color = new Color(colors[activeSequence[positionInSequence]].color.r, colors[activeSequence[positionInSequence]].color.g, colors[activeSequence[positionInSequence]].color.b, 1f);
+
+                    stayLitCounter = stayLit;
+                    shouldBeLit = true;
+
+                    gameActive = false;
+
+                }
+            }
+            else
+            {
+                Debug.Log("Wrong");
+
+                gameActive = false;
+            }
+        }   
+    }
+}
