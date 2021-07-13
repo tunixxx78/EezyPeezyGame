c6aaddbddc7a97bdd0b68a5f58427a11dd9113f7
Taru Vikström <taru.vikstrom@yahoo.com>
2021-07-13 12:38:04 +0300

    Main menu interactables + script, elevator prefab to type slice and stretched a bit, drawing board moved a bit

---
+++ EezyPeezyGame/Assets/Scripts/InteractableObject2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject2 : MonoBehaviour
+{
+    /*
+     This is a simple script for interactable objects in scenes that are animated when they are clicked.
+     Give the script to the wanted object that has an animation, set the variables in Unity for the wanted animation clip and clips name.
+    */
+
+    private Animator animator;
+    public AnimationClip animClip;
+    public string animName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Debug.Log("klik");
+            animator.Play(animName);
+        }
+    }
+}
---
+++ EezyPeezyGame/Assets/Scripts/UI/Cursor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    public Texture2D cursorDefault, cursorInteractable;
+    public CursorMode CursorMode = CursorMode.Auto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
