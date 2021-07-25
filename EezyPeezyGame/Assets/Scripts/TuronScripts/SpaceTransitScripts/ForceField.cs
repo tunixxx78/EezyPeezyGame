fe2fa11ebef429c0e80678740197526e12493cb9
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-07-25 09:38:58 +0300

    Updates for SpaceTransit scene
    Added forceField for rocket and functionality for that.

---
+++ EezyPeezyGame/Assets/Scripts/TuronScripts/SpaceTransitScripts/ForceField.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ForceField : MonoBehaviour
+{
+    public static int theForceField;
+    [SerializeField] GameObject fullField, mediumFiel, lowField, tryAgainMessage, spaceShip;
+
+    private void Start()
+    {
+        theForceField = 3;
+    }
+
+    private void Update()
+    {
+        if(theForceField > PlayerPrefs.GetInt("StrenghtOfField", 0))
+        {
+            PlayerPrefs.SetInt("StreghtOfField", theForceField);
+        }
+
+        if(theForceField == 3)
+        {
+            fullField.SetActive(true);
+        }
+        if(theForceField == 2)
+        {
+            fullField.SetActive(false);
+            mediumFiel.SetActive(true);
+        }
+        if(theForceField == 1)
+        {
+            fullField.SetActive(false);
+            mediumFiel.SetActive(false);
+            lowField.SetActive(true);
+        }
+        if (theForceField == 0)
+        {
+            Invoke("TryAgain", 2f);
+            spaceShip.SetActive(false);
+            tryAgainMessage.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Asteroid"))
+        {
+            theForceField -= 1;
+        }
+        if (collision.CompareTag("Obstacle"))
+        {
+            theForceField -= 1;
+        }
+    }
+
+    private void TryAgain()
+    {
+        SceneManager.LoadScene("SpaceTransit");
+    }
+}
