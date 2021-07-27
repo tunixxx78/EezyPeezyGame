2843083f4a5e37dc146b4ec34d55497a9d443776
Taru Vikström <taru.vikstrom@yahoo.com>
2021-07-27 11:48:41 +0300

    ESC button made into UI button as well (takes you to the previous game view)

--- EezyPeezyGame/Assets/Scripts/InputManager.cs
+++ EezyPeezyGame/Assets/Scripts/InputManager.cs
@@ -16,38 +16,43 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            //map.SetActive(!map.activeInHierarchy);
-        }
        
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            if (scene.name == "RocketCockpit")
-            {
-                SceneManager.LoadScene("RocketLobby");
-            }
-            else if (scene.name == "HeadQuarters")
-            {
-                SceneManager.LoadScene("HomePlanet");
-            }
-            else if (scene.name == "RocketArcade")
-            {
-                SceneManager.LoadScene("RocketLobby");
-            }
-            else if (scene.name == "RocketEngineFloor")
-            {
-                SceneManager.LoadScene("RocketLobby");
-            }
-            else if (scene.name == "FuelPipes")
-            {
-                SceneManager.LoadScene("RocketEngineFloor");
-            }
-            else
-            {
-                SceneManager.LoadScene(scene.buildIndex - 1);
-            }
+            BackToPreviousScene();
         }
 
+    }
+
+    public void BackToPreviousScene()
+    {
+        if (scene.name == "RocketCockpit")
+        {
+            SceneManager.LoadScene("RocketLobby");
+        }
+        else if (scene.name == "HeadQuarters")
+        {
+            SceneManager.LoadScene("HomePlanet");
+        }
+        else if (scene.name == "RocketArcade")
+        {
+            SceneManager.LoadScene("RocketLobby");
+        }
+        else if (scene.name == "RocketEngineFloor")
+        {
+            SceneManager.LoadScene("RocketLobby");
+        }
+        else if (scene.name == "FuelPipes")
+        {
+            SceneManager.LoadScene("RocketEngineFloor");
+        }
+        else if (scene.name == "SimonSays")
+        {
+            SceneManager.LoadScene("RocketCockpit");
+        }
+        else
+        {
+            SceneManager.LoadScene(scene.buildIndex - 1);
+        }
     }
 }
