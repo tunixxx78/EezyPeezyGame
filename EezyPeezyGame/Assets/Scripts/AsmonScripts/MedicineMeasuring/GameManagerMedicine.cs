39c1e5a60530800e34d5785a393598d1870ad53d
centyh <97asmo@gmail.com>
2021-08-19 18:18:44 +0300

    MedicineMeasure

---
+++ EezyPeezyGame/Assets/Scripts/AsmonScripts/MedicineMeasuring/GameManagerMedicine.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerMedicine : MonoBehaviour
+{
+    [SerializeField]private Image glassOfWater;
+
+    public float maxAmount = 100f;
+    public float currentAmount = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        if (Input.GetKeyDown("Space"))
+        {
+
+        }
+    }
+}
