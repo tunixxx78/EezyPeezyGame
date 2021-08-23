06d7c6791ff88419a78ca62e1bf96ffe5ad885f2
Taru Vikström <taru.vikstrom@yahoo.com>
2021-08-23 18:50:48 +0300

    SavingSystem changed

--- EezyPeezyGame/Assets/Scripts/DataHolder.cs
+++ EezyPeezyGame/Assets/Scripts/DataHolder.cs
@@ -10,7 +10,7 @@ public class DataHolder : MonoBehaviour
 {
     public static DataHolder dataHolder;
     public string currentScene;
-    // public int score, foundAliens;
+    public int foundAliens;
 
     public bool tutorialDone, lobbyDone, cockpitDone, dashboardDone,
         phoneCallDone, headquartersDone, labyrinthDone, engineFloorDone, 
@@ -31,11 +31,72 @@ public class DataHolder : MonoBehaviour
         }
     }
 
-    private void Update()
+    public void Save()
     {
+        Debug.Log("Saving data.");
+
         currentScene = SceneManager.GetActiveScene().name;
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+        PlayerData data = new PlayerData();
+
+        data.currentScene = currentScene;
+        data.foundAliens = foundAliens;
+
+        data.tutorialDone = tutorialDone;
+        data.lobbyDone = lobbyDone;
+        data.cockpitDone = cockpitDone;
+        data.dashboardDone = dashboardDone;
+        data.phoneCallDone = phoneCallDone;
+        data.headquartersDone = headquartersDone;
+        data.labyrinthDone = labyrinthDone;
+        data.engineFloorDone = engineFloorDone;
+        data.FuelPipesDone = FuelPipesDone;
+        data.engineStartDone = engineStartDone;
+        data.spaceTransitDone = spaceTransitDone;
+        data.gridNavigationDone = gridNavigationDone;
+        data.medicineMeasureDone = medicineMeasureDone;
+        data.newtonTreatedDone = newtonTreatedDone;
+        bf.Serialize(file, data);
+        file.Close();
+
     }
 
+    public void Load()
+    {
+        Debug.Log("No path found.");
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            currentScene = data.currentScene;
+            foundAliens = data.foundAliens;
+
+            tutorialDone = data.tutorialDone;
+            lobbyDone = data.lobbyDone;
+            cockpitDone = data.cockpitDone;
+            dashboardDone = data.dashboardDone;
+            phoneCallDone = data.phoneCallDone;
+            headquartersDone = data.headquartersDone;
+            labyrinthDone = data.labyrinthDone;
+            engineFloorDone = data.engineFloorDone;
+            FuelPipesDone = data.FuelPipesDone;
+            engineStartDone = data.engineStartDone;
+            spaceTransitDone = data.spaceTransitDone;
+            gridNavigationDone = data.gridNavigationDone;
+            medicineMeasureDone = data.medicineMeasureDone;
+            newtonTreatedDone = data.newtonTreatedDone;
+
+            Debug.Log("Loaded gamedata.");
+
+            SceneManager.LoadScene(currentScene);
+        }
+
+    }
 }
 
 [Serializable]
@@ -43,7 +104,7 @@ public class DataHolder : MonoBehaviour
 class PlayerData
 {
     public string currentScene;
-    public int score, foundAliens;
+    public int foundAliens;
 
     public bool tutorialDone, lobbyDone, cockpitDone, dashboardDone,
         phoneCallDone, headquartersDone, labyrinthDone, engineFloorDone, 
---
+++ EezyPeezyGame/Assets/Scripts/UI/DataHolderSaver.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class DataHolderSaver : MonoBehaviour
+{
+    public void Save()
+    {
+        Debug.Log("Saving data.");
+
+        DataHolder.dataHolder.currentScene = SceneManager.GetActiveScene().name;
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+        PlayerData data = new PlayerData();
+
+        data.currentScene = DataHolder.dataHolder.currentScene;
+        data.foundAliens = DataHolder.dataHolder.foundAliens;
+
+        data.tutorialDone = DataHolder.dataHolder.tutorialDone;
+        data.lobbyDone = DataHolder.dataHolder.lobbyDone;
+        data.cockpitDone = DataHolder.dataHolder.cockpitDone;
+        data.dashboardDone = DataHolder.dataHolder.dashboardDone;
+        data.phoneCallDone = DataHolder.dataHolder.phoneCallDone;
+        data.headquartersDone = DataHolder.dataHolder.headquartersDone;
+        data.labyrinthDone = DataHolder.dataHolder.labyrinthDone;
+        data.engineFloorDone = DataHolder.dataHolder.engineFloorDone;
+        data.FuelPipesDone = DataHolder.dataHolder.FuelPipesDone;
+        data.engineStartDone = DataHolder.dataHolder.engineStartDone;
+        data.spaceTransitDone = DataHolder.dataHolder.spaceTransitDone;
+        data.gridNavigationDone = DataHolder.dataHolder.gridNavigationDone;
+        data.medicineMeasureDone = DataHolder.dataHolder.medicineMeasureDone;
+        data.newtonTreatedDone = DataHolder.dataHolder.newtonTreatedDone;
+        bf.Serialize(file, data);
+        file.Close();
+
+    }
+}
