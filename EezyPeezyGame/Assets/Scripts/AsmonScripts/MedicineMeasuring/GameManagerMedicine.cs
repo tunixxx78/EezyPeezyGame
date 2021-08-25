9654a8e50e3daf612e496293cfe31cf5364c26bf
centyh <97asmo@gmail.com>
2021-08-25 18:37:20 +0300

    MedicineMeasure update

--- EezyPeezyGame/Assets/Scripts/AsmonScripts/MedicineMeasuring/GameManagerMedicine.cs
+++ EezyPeezyGame/Assets/Scripts/AsmonScripts/MedicineMeasuring/GameManagerMedicine.cs
@@ -6,37 +6,38 @@ using TMPro;
 
 public class GameManagerMedicine : MonoBehaviour
 {
-    [SerializeField] private GameObject emptyVial;
+    [SerializeField] private GameObject emptyVial, gameFinished, gameFailed, mix, particleSmoke;
     [SerializeField] private TMP_Text[] medicineText;
     [SerializeField] private Button[] medicineJar;
     [SerializeField] private Button[] whichJar;
+
+    public AudioSource dropSound;
 
     private PipetteCursor pcScript;
 
     private Button selectedButton;
 
     private float blueDropCount;
-    public float redDropCount;
+    private float redDropCount;
     private float yellowDropCount;
 
     public float redDropsNeeded;
     public float blueDropsNeeded;
     public float yellowDropsNeeded;
 
-    public int redDrops;
-    public int blueDrops;
-    public int yellowDrops;
-
     public bool redActive = false;
     public bool blueActive = false;
     public bool yellowActive = false;
 
 
-    private bool activeMedicine;
+    private bool activeJar;
+
+    
 
 
     void Start()
     {
+        dropSound = GetComponent<AudioSource>();
         pcScript = GetComponent<PipetteCursor>();
         JarDropCount();
     }
@@ -62,36 +63,86 @@ public class GameManagerMedicine : MonoBehaviour
         yellowDropsNeeded = yellowDropCount;
     }
 
-    public void AddingDrops()
-    {
-        
-    }
-
-    
-
 
 
     public void Drops()
     {
-        redDropCount--;
-        medicineText[0].text = redDropCount.ToString("" + redDropCount);
-        Debug.Log("Hyvä hyvä");
-        if (redDropCount == 0)
+        if(redActive)
         {
-            Debug.Log("VITTU JEE");
-            redActive = false;
+            dropSound.Play();
+            redDropCount--;
+            Debug.Log("luvut laskee");
+            if (redDropCount == 0)
+            {
+                Debug.Log("VITTU JEE");
+                redActive = false;  
+            }
         }
-        
 
-        //blueDropCount--;
-        //medicineText[1].text = blueDropCount.ToString("" + blueDropCount);
-        //Debug.Log("SININEN");
-        //if(blueDropCount == 0)
-        //{
-        //    Debug.Log("OUJEE");
-        //    blueActive = false;     
-        //}
+        if (blueActive)
+        {
+            dropSound.Play();
+            blueDropCount--;
+            Debug.Log("siniset laskee");
+            if(blueDropCount == 0)
+            {
+                Debug.Log("KOVA!");
+                blueActive = false;
+
+            }
+        }
+
+        if (yellowActive)
+        {
+            dropSound.Play();
+            yellowDropCount--;
+            Debug.Log("keltaset laskee");
+            if(yellowDropCount == 0)
+            {
+                Debug.Log("JEEEE");
+                yellowActive = false;
+            }
+        }
     }
 
+    public void CheckIfRightFormula()
+    {
+        if(redDropCount == 0 && blueDropCount == 0 && yellowDropCount == 0)
+        {
+            gameFinished.SetActive(true);
+        }
+
+        else
+        {
+            gameFailed.SetActive(true);
+            particleSmoke.SetActive(true);
+            JarDropCount();
+        }
+    }
+
+
+    public void ActiveJarRed()
+    {
+        redActive = true;
+        blueActive = false;
+        yellowActive = false;
+        
+    }
+
+    public void ActiveJarBlue()
+    {
+        redActive = false;
+        blueActive = true;
+        yellowActive = false;
+        return;
+    }
+
+    public void ActiveJarYellow()
+    {
+        redActive = false;
+        blueActive = false;
+        yellowActive = true;
+        return;
+    }
 
 }
