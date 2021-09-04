51c59e4877b26b1265e6e62989c207dd837e08fe
Taru Vikström <taru.vikstrom@yahoo.com>
2021-09-04 15:49:53 +0300

    arcade labyrinth done

---
+++ EezyPeezyGame/Assets/Scripts/UI/LabyrinthLevel.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This script is for Arcade version of the labyrinth. It's attached to the Level changing button.
+
+public class LabyrinthLevel : MonoBehaviour
+{
+    public GameObject[] labTemplates;
+    private int index;
+
+    private void Start()
+    {
+        index = 0;
+        labTemplates[index].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            ChangeTemplate();
+        }
+
+    }
+
+    public void ChangeTemplate()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("Mask"));
+        Destroy(GameObject.FindGameObjectWithTag("Needle"));
+        Destroy(GameObject.FindGameObjectWithTag("Pill"));
+        Destroy(GameObject.FindGameObjectWithTag("Stetoscope"));
+        Destroy(GameObject.FindGameObjectWithTag("Thermometer"));
+
+        if (index < labTemplates.Length - 1)
+        {
+            labTemplates[index].SetActive(false);
+            index++;
+            labTemplates[index].SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene("Labyrinth2");
+        }
+    }
+}
