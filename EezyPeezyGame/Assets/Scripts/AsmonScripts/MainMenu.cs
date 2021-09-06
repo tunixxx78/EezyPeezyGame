535037f91372328fe174881939f974750e7e1531
Taru Vikström <taru.vikstrom@yahoo.com>
2021-09-06 19:22:14 +0300

    Credits in main menu and hyperlink testing

--- EezyPeezyGame/Assets/Scripts/AsmonScripts/MainMenu.cs
+++ EezyPeezyGame/Assets/Scripts/AsmonScripts/MainMenu.cs
@@ -9,7 +9,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public static MainMenu instance;
-    [SerializeField] GameObject loginScreen, registerScreen, startGameScreen, avatar;
+    [SerializeField] GameObject loginScreen, registerScreen, startGameScreen, creditsPanel, avatar;
     [SerializeField] InputField userInput, passwordInput;
 
     private void Awake()
@@ -45,5 +45,18 @@ public class MainMenu : MonoBehaviour
         startGameScreen.SetActive(true);
         loginScreen.SetActive(false);
     }
+
+    public void Credits()
+    {
+        if(creditsPanel.activeSelf == true)
+        {
+            creditsPanel.SetActive(false);
+        }
+        else
+        {
+            creditsPanel.SetActive(true);
+        }
+    }
+
 
 }
